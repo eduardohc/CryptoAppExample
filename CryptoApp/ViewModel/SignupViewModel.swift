1e1f879effcf34f7897d95fe050cfa21277c6c80
Eduardo Hernandez <lalohernandez93@gmail.com>
2023-07-06 01:52:38 -0600

    Add signup response into a thread

--- CryptoApp/ViewModel/SignupViewModel.swift
+++ CryptoApp/ViewModel/SignupViewModel.swift
@@ -9,19 +9,24 @@ import Foundation
 
 class SignupViewModel: ObservableObject {
     @Published var signupModel = SignupResponseModel()
+    @Published var isLoading = false
     
     let apiClient = APIClient()
     
     func signup(url: String, httpMethod: HTTPMethod, signupData: SignupDataModel) {
+        isLoading = true
         
         apiClient.sendRequest(urlString: url, method: httpMethod, data: signupData) { ( result: Result<SignupResponseModel, APIError>) in
-            switch result {
-            case .success(let responseData):
-                // Handle the successful response
-                print(responseData)
-            case .failure(let error):
-                // Handle the error
-                print(error)
+            
+            DispatchQueue.main.async {
+                self.isLoading = false
+                
+                switch result {
+                case .success(let responseData):
+                    self.signupModel = responseData
+                case .failure(let error):
+                    print(error)
+                }
             }
         }
     }
